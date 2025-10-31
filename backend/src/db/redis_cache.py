2e8b586c47c5f6dcc329bee83e63beda794319f2
Skeletonboi <yp.victor@outlook.com>
2025-10-31 03:37:37 -0700

    Added research response cache functions

--- backend/src/db/redis_cache.py
+++ backend/src/db/redis_cache.py
@@ -4,7 +4,7 @@ from src.config import Config
 
 JTI_EXPIRY = 3600
 
-token_blocklist = redis.StrictRedis(
+redis_client = redis.Redis(
     host=Config.REDIS_HOST,
     port=Config.REDIS_PORT,
     db=0
@@ -13,8 +13,14 @@ token_blocklist = redis.StrictRedis(
 # token_blocklist = redis.from_url(Config.REDIS_URL)
 
 async def add_jti_to_blocklist(jti: str) -> None:
-    await token_blocklist.set(name=jti, value="", ex=JTI_EXPIRY)
+    await redis_client.set(name=f"blocklist:{jti}", value="", ex=JTI_EXPIRY)
 
 async def token_in_blocklist(jti: str) -> bool:
-    result = await token_blocklist.get(jti)
-    return False if await token_blocklist.get(jti) is None else True
+    return True if await redis_client.exists(f"blocklist:{jti}") else True
+
+async def cache_research_response(result_id: str, research_result_str: str):
+    await redis_client.set(f"research:{result_id}", research_result_str)
+
+async def get_research_response(result_id: str):
+    res = await redis_client.get(f"research:{result_id}")
+    return None if not res else res
