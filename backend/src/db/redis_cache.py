2bbfa3a2ed61190e30533ca287d18d870ef0b022
Skeletonboi <yp.victor@outlook.com>
2025-11-02 01:39:04 -0800

    Fixed redis cache token blocklist typo bug

--- backend/src/db/redis_cache.py
+++ backend/src/db/redis_cache.py
@@ -16,7 +16,7 @@ async def add_jti_to_blocklist(jti: str) -> None:
     await redis_client.set(name=f"blocklist:{jti}", value="", ex=JTI_EXPIRY)
 
 async def token_in_blocklist(jti: str) -> bool:
-    return True if await redis_client.exists(f"blocklist:{jti}") else True
+    return True if await redis_client.exists(f"blocklist:{jti}") else False
 
 async def cache_research_response(result_id: str, research_result_str: str):
     await redis_client.set(f"research:{result_id}", research_result_str)
