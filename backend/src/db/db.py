99b0ee19d3a54446ab4d362096eec82b15bb2b8d
Skeletonboi <yp.victor@outlook.com>
2025-11-26 03:16:04 -0800

    Updated async engine instantiation to use sqlalchemy's create_async_engine for proper connection lifecycle management, pool_pre_ping to keep session active for more than one use per request

--- backend/src/db/db.py
+++ backend/src/db/db.py
@@ -1,16 +1,17 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.engine import Engine
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from typing import AsyncGenerator
 from contextlib import asynccontextmanager
 
 from src.config import Config
 from src.db.base_model import BaseModel
 
-async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
-
-Session = sessionmaker(bind=async_engine, class_=AsyncSession)
+async_engine = create_async_engine(
+    url=Config.DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True
+)
+Session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
 async def init_db():
     async with async_engine.begin() as conn:
